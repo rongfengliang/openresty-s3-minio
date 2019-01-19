9464e43679aae5f66e2f958f68fb56b02676dafc
dalongrong <1141591465@qq.com>
2019-01-19 22:04:07 +0800

    change export

--- code/s3/init.lua
+++ code/s3/init.lua
@@ -3,7 +3,11 @@ local s3 = require "resty.s3"
 function init()
     local s3, err = s3:new("dalongapp", "dalongapp")
     final_url, err = s3:upload_url("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1753254544,2672940031&fm=26&gp=0.jpg", "images", "70cFvXSh_Q1YnxGkpoWK1HF6hhy")
+    if err~=nil then
+      ngx.say(err)
+    else 
     ngx.say(final_url);
+    end
 end
 
 return init
