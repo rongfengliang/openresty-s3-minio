2e8f3a755b972cae0b5515e6593dbe72d91092dd
dalongrong <1141591465@qq.com>
2019-01-19 22:26:24 +0800

    change export

--- code/s3/init.lua
+++ code/s3/init.lua
@@ -2,7 +2,7 @@ local s3 = require "resty.s3"
 
 function init()
     local s3, err = s3:new("dalongapp", "dalongapp")
-    final_url, err = s3:upload_url("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1753254544,2672940031&fm=26&gp=0.jpg", "images", "70cFvXSh_Q1YnxGkpoWK1HF6hhy")
+    final_url, err = s3:upload_url("https://nginx.org/nginx.png", "images", "70cFvXSh_Q1YnxGkpoWK1HF6hhy")
     if err~=nil then
       ngx.say(err)
     else 
